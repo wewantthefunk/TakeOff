8656cb695923290b4186343d5f2a9bbc7b9ff239
oak-and-lion <oakandlionsociety@gmail.com>
2020-08-09 22:12:00 -0400

    Initial Commit

---
+++ Take Off/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Take Off
+//
+//  Created by MyMac on 8/9/20.
+//  Copyright © 2020 White Paladin Games. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Take OffTests/Take_OffTests.swift
@@ -0,0 +1,34 @@
+//
+//  Take_OffTests.swift
+//  Take OffTests
+//
+//  Created by MyMac on 8/9/20.
+//  Copyright © 2020 White Paladin Games. All rights reserved.
+//
+
+import XCTest
+@testable import Take_Off
+
+class Take_OffTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
